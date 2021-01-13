9779fd2b0738171ed3376192308830cdd3df4d5e
Aakash <arana@ex2india.com>
2021-01-13 23:35:22 +0530

    Updated version..

--- task2.js
+++ task2.js
@@ -1,129 +1,132 @@
-let submitEl = document.querySelector(".submit");
-let data = [];
-
-submitEl.addEventListener('click', function(){
-    data = document.querySelector(".input").value.split(' ');
-
-    if(data.length === 1 && data[0] === "") {
-        alert('No DATA! No Analysis!');
-    }
-
-    for(let i = 0; i < data.length; i++) {
-         data[i] = Number(data[i].trim());
-    }
-});
-
 // Function to sort the data in ascending order
 function upData(data) {
-    let newData = data.sort(function(a, b)
-    {
-        return a - b; 
-    });
+  let newData = data.sort(function (a, b) {
+    return a - b;
+  });
 
-    return newData;
+  return newData;
 }
 
 // Function to sort the data in descending order
 function downData(data) {
-    let newData = data.sort(function(a, b)
-    {
-        return b - a; 
-    });
+  let newData = data.sort(function (a, b) {
+    return b - a;
+  });
 
-    return newData;
+  return newData;
 }
 
 // Function to find the max valued data
 function maxData(data) {
-    let maxEle = Math.max(...data);
-    return maxEle;
+  let maxEle = Math.max(...data);
+  return maxEle;
 }
 
 // Function to find the min valued data
 function minData(data) {
-    let minEle = Math.min(...data);
+  let minEle = Math.min(...data);
 
-    return minEle;
+  return minEle;
 }
 
 // Function to get the sum of the data
 function sumOfData(data) {
-    var sum = data.reduce(function(a, b)
-    {
-        return a + b;
-    });
+  var sum = data.reduce(function (a, b) {
+    return a + b;
+  });
 
-    return sum;
+  return sum;
 }
 
 // Function to find the median of the data
 function medianOfData(data) {
-    let arr1 = data.sort(function(a, b){
-        a - b;
-    });
-    let mid = Math.floor(arr1.length/2);
+  let arr1 = data.sort(function (a, b) {
+    a - b;
+  });
+  let mid = Math.floor(arr1.length / 2);
 
-    if(arr1.length % 2){
-        return arr1[mid];
-    }
-    else { 
-        return (arr1[mid - 1] + arr1[mid])/2;
-    }
-
+  if (arr1.length % 2) {
+    return arr1[mid];
+  } else {
+    return (arr1[mid - 1] + arr1[mid]) / 2;
+  }
 }
+
+// Function to calculate the mean of the data
 function meanOfData(data) {
-    let sum = sumOfData(data);
-    let length = data.length;
-    let mean = (sum / length).toFixed(3);
-   
-    return mean;
+  let sum = 0;
+  if (mymap.get(4) == undefined) {
+    sum = mymap.set(4, sumOfData(data));
+  }
+
+  let length = data.length;
+  let mean = (mymap.get(4) / length).toFixed(3);
+
+  return mean;
 }
 
+// Function to calculate the standard deviation
 function stdev(data) {
+  const mean = meanOfData(data);
+  let sumOfSquares = 0;
 
-    const mean = meanOfData(data);
-    let sumOfSquares = 0;
+  for (let i = 0; i < data.length; i++) {
+    sumOfSquares += (data[i] - mean) * (data[i] - mean);
+  }
+  const val = sumOfSquares / data.length;
 
-    for(let i = 0; i < data.length; i++){
-        sumOfSquares += (data[i] - mean)*(data[i] - mean);
-    }
-    const val = sumOfSquares / data.length;
-
-    return Math.sqrt(val).toFixed(3);
+  return Math.sqrt(val).toFixed(3);
 }
 
-var buttonEl = document.getElementsByTagName("button");
+// Main JS
+let submitEl = document.querySelector(".submit");
+let data = [];
+let mymap = new Map();
 
-var divEl = document.getElementById("result");
+submitEl.addEventListener("click", function () {
+  data = document.querySelector(".input").value.split(" ");
 
-for(let i = 0; i < buttonEl.length; i++)
-{
-    buttonEl[i].addEventListener("click", function() {
-        var op = buttonEl[i].name;
+  if (data.length === 1 && data[0] === "") {
+    alert("No DATA! No Analysis!");
+  }
 
-        if(op === "up") {
-            divEl.innerHTML = upData(data); 
-        }
-        else if(op === "down") {
-            divEl.innerHTML = downData(data); 
-        }
-        else if(op === "max") {
-            divEl.innerHTML = maxData(data); 
-        }
-        else if(op === "min") {
-            divEl.innerHTML = minData(data); 
-        }
-        else if(op === "sum") {
-            divEl.innerHTML = sumOfData(data); 
-        }
-        else if(op === "mean") {
-            divEl.innerHTML = meanOfData(data); 
-        }
-        else if(op === "median") {
-            divEl.innerHTML = medianOfData(data); 
-        }
-        else if(op === "std") {
-            divEl.innerHTML = stdev(data); 
-        }
-    } );
-}
+  for (let i = 0; i < data.length; i++) {
+    data[i] = Number(data[i].trim());
+  }
+
+  document.querySelector(".hidden").style.display = "";
+
+  document.querySelector(".result").style.display = "none";
+
+  mymap.clear();
+});
+
+var selectEl = document.querySelector("select");
+var divEl = document.querySelector(".result");
+
+selectEl.addEventListener("change", function () {
+  divEl.style.display = "";
+  var op = selectEl.value;
+
+  if (mymap.get(op) != undefined) {
+    divEl.innerHTML = mymap.get(op);
+  } else if (op == 0) {
+    mymap.set(op, upData(data));
+  } else if (op == 1) {
+    mymap.set(op, downData(data));
+  } else if (op == 2) {
+    mymap.set(op, maxData(data));
+  } else if (op == 3) {
+    mymap.set(op, minData(data));
+  } else if (op == 4) {
+    mymap.set(op, sumOfData(data));
+  } else if (op == 5) {
+    mymap.set(op, meanOfData(data));
+  } else if (op == 6) {
+    mymap.set(op, medianOfData(data));
+  } else if (op == 7) {
+    mymap.set(op, stdev(data));
+  }
+
+  divEl.innerHTML = "Result : " + mymap.get(op);
+});
